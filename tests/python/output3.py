b37d2cc1b5d584638754b923262b9af74c1d07f9
mandorodriguez+Ubuntu.VM.Home.i686 <mandorodriguez+Ubuntu.VM.Home.i686@gmail.com>
2011-03-22 06:26:00 +0000

    1. Added two more output test scripts cooresponding to the existing tests in ssp.

---
+++ tests/python/output3.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+"""
+
+"""
+import pdb
+import os
+
+from test_library import add_sspy_path
+
+add_sspy_path()
+
+from sspy import SSPy 
+
+
+scheduler = SSPy(verbose=True)
+
+try:
+    
+    scheduler.Load("./yaml/output3.yml")
+
+except Exception, e:
+
+    print e
+
+
+scheduler.Run()
