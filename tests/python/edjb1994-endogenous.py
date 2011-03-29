9176856b7922f3607beec068854b0abb849825d9
mandorodriguez <mandorodriguez@gmail.com>
2011-03-29 21:47:09 +0000

    1. Added purkinje cell test script.
    2. Added more specification files for testing spikes and pools.

---
+++ tests/python/edjb1994-endogenous.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+"""
+
+"""
+import pdb
+import os
+
+os.environ['NEUROSPACES_NMC_MODELS']='/usr/local/neurospaces/models/library'
+
+from test_library import add_sspy_path
+
+add_sspy_path()
+
+from sspy import SSPy 
+
+
+scheduler = SSPy()
+
+try:
+    
+    scheduler.Load("./yaml/purkinje/edsjb1994-endogenous.yml")
+
+except Exception, e:
+
+    print e
+
+
+scheduler.Run()
+
+print "Done!"
+
