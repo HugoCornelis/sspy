50b14f69cae604388136e4539b50010912a6ca31
mandorodriguez <mandorodriguez@gmail.com>
2011-05-03 18:40:26 +0000

    1. Added an info file for some developer info.

--- genesis3/sspy/shell/__init__.py
+++ genesis3/sspy/shell/__init__.py
@@ -15,6 +15,12 @@ class GShell(cmd.Cmd):
     def __init__(self, scheduler=None,
                  intro='Welcome to the GENESIS 3 shell. Type help or ? to list commands.\n',
                  prompt='genesis> '):
+        """
+
+        @param scheduler The sspy schedule object to wrap around
+        @param intro The intro text to use when starting the shell
+        @param prompt The text for the command line prompt.
+        """
 
         cmd.Cmd.__init__(self)
 
@@ -26,21 +32,31 @@ class GShell(cmd.Cmd):
         
         self._scheduler = scheduler
 
+#---------------------------------------------------------------------------
+#----                           Commands                              ------
+#---------------------------------------------------------------------------
 
     def do_hello(self, arg):
         print "hello again", arg, "!"
 
+#---------------------------------------------------------------------------
+
     def help_hello(self):
         print "syntax: hello [message]",
         print "-- prints a hello message"
-        
+
+#---------------------------------------------------------------------------
+
     def do_quit(self, arg):
         sys.exit(1)
+
+#---------------------------------------------------------------------------
 
     def help_quit(self):
         print "syntax: quit",
         print "-- terminates the application"
 
+#---------------------------------------------------------------------------
     # shortcuts
     do_q = do_quit
     
