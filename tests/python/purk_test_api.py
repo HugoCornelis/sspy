9468f27d87e9356b78e023cc4b3cefb66d77386d
mandorodriguez <mandorodriguez@gmail.com>
2011-06-07 22:19:19 +0000

    1. Updated purk test api script to use the identical time step, and outputs from the purk_test.yml yaml declaration.

--- tests/python/purk_test_api.py
+++ tests/python/purk_test_api.py
@@ -26,9 +26,9 @@ my_model_container = scheduler.CreateService(name="My Model Container",
                                              type="model_container",
                                              verbose=True)
 
-my_model_container.Load('tests/cells/purk_test_segment.ndf')
+my_model_container.Load('tests/cells/purk_test.ndf')
 
-my_model_container.SetParameter('/purk_test_segment/segments/test_segment',
+my_model_container.SetParameter('/purk_test_segment/segments/soma',
                                 'INJECT',
                                 2e-09)
 
@@ -38,10 +38,10 @@ my_model_container.SetParameter('/purk_test_segment/segments/test_segment',
 my_heccer = scheduler.CreateSolver('My solver', 'heccer', verbose=True)
 
 # Sets the segment of the model to run from
-my_heccer.SetModelName('/purk_test_segment')
+my_heccer.SetModelName('/purk_test')
 
 # set the timestep for the entire scheduler (solvers, inputs and outputs)
-my_heccer.SetTimeStep(1e-05)
+my_heccer.SetTimeStep(2e-05)
 
 
 #
@@ -49,15 +49,39 @@ my_heccer.SetTimeStep(1e-05)
 #
 my_output = scheduler.CreateOutput('My output object', 'double_2_ascii')
 
-my_output.AddOutput('/purk_test_segment/segments/test_segment', 'Vm')
-my_output.AddOutput('/purk_test_segment/segments/test_segment/cat/cat_gate_activation', 'state_m')
-my_output.AddOutput('/purk_test_segment/segments/test_segment/cat/cat_gate_inactivation', 'state_h')
-my_output.AddOutput('/purk_test_segment/segments/test_segment/kdr', 'state_m')
-my_output.AddOutput('/purk_test_segment/segments/test_segment/kdr', 'state_h')
-my_output.AddOutput('/purk_test_segment/segments/test_segment/nap', 'state_n')
-my_output.AddOutput('/purk_test_segment/segments/test_segment/naf', 'state_m')
-my_output.AddOutput('/purk_test_segment/segments/test_segment/naf', 'state_h')
+my_output.SetFilename('/tmp/output')
 
-scheduler.Run(steps=10000)
+my_output.AddOutput('/purk_test/segments/soma', 'Vm')
+my_output.AddOutput('/purk_test/segments/soma/ca_pool', 'Ca')
+my_output.AddOutput('/purk_test/segments/soma/km', 'state_n')
+my_output.AddOutput('/purk_test/segments/soma/kdr', 'state_m')
+my_output.AddOutput('/purk_test/segments/soma/kdr', 'state_h')
+my_output.AddOutput('/purk_test/segments/soma/ka', 'state_m')
+my_output.AddOutput('/purk_test/segments/soma/ka', 'state_h')
+my_output.AddOutput('/purk_test/segments/soma/kh', 'state_m')
+my_output.AddOutput('/purk_test/segments/soma/kh', 'state_h')
+my_output.AddOutput('/purk_test/segments/soma/nap', 'state_n')
+my_output.AddOutput('/purk_test/segments/soma/naf', 'state_m')
+my_output.AddOutput('/purk_test/segments/soma/naf', 'state_h')
+my_output.AddOutput('/purk_test/segments/soma/cat/cat_gate_activation', 'state_m')
+my_output.AddOutput('/purk_test/segments/soma/cat/cat_gate_activation', 'state_h')
+my_output.AddOutput('/purk_test/segments/main[0]', 'Vm')
+my_output.AddOutput('/purk_test/segments/main[0]/ca_pool', 'Ca')
+my_output.AddOutput('/purk_test/segments/main[0]/cat/cat_gate_activation', 'state_m')
+my_output.AddOutput('/purk_test/segments/main[0]/cat/cat_gate_inactivation', 'state_h')
+my_output.AddOutput('/purk_test/segments/main[0]/cap/cap_gate_activation', 'state_m')
+my_output.AddOutput('/purk_test/segments/main[0]/cap/cap_gate_inactivation', 'state_h')
+my_output.AddOutput('/purk_test/segments/main[0]/km', 'state_n')
+my_output.AddOutput('/purk_test/segments/main[0]/kdr', 'state_m')
+my_output.AddOutput('/purk_test/segments/main[0]/kdr', 'state_h')
+my_output.AddOutput('/purk_test/segments/main[0]/ka', 'state_m')
+my_output.AddOutput('/purk_test/segments/main[0]/ka', 'state_h')
+my_output.AddOutput('/purk_test/segments/main[0]/kc', 'state_m')
+my_output.AddOutput('/purk_test/segments/main[0]/kc', 'state_h')
+my_output.AddOutput('/purk_test/segments/main[0]/k2', 'state_m')
+my_output.AddOutput('/purk_test/segments/main[0]/k2', 'state_h')
+
+
+scheduler.Run(steps=2500)
 
 print "Done!"
