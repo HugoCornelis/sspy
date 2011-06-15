97fbfb6bf2f7cc3ce31f84ca860796ae9572361a
mandorodriguez+Ubuntu.VM.Home.i686 <mandorodriguez+Ubuntu.VM.Home.i686@gmail.com>
2011-06-15 07:10:42 +0000

    1. Added test for purk_test_soma_aggregators via sspy on the command line.
    2. Added script and test for purk_test_soma_aggregators for sspy
    via API. 
    
    3. Removed perl hash from purk_test_soma_aggregators declaration.

---
+++ tests/python/purk_test_soma_aggregators_api.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+"""
+
+"""
+import pdb
+import os
+import sys
+
+os.environ['NEUROSPACES_NMC_MODELS']='/usr/local/neurospaces/models/library'
+
+from test_library import add_sspy_path
+
+add_sspy_path()
+
+from sspy import SSPy 
+
+
+scheduler = SSPy(verbose=True)
+
+my_model_container = None
+#
+# Create a model container service and load an ndf file
+#
+    
+my_model_container = scheduler.CreateService(name="My Model Container",
+                                             type="model_container",
+                                             verbose=True)
+
+my_model_container.Load('tests/cells/purk_test_soma.ndf')
+
+my_model_container.SetParameter('/purk_test_soma/segments/soma',
+                                'INJECT',
+                                2e-09)
+
+#
+# Must create solver.
+#
+my_heccer = scheduler.CreateSolver('My solver', 'heccer', verbose=True)
+
+# Sets the segment of the model to run from
+my_heccer.SetModelName('/purk_test_soma')
+
+# set the timestep for the entire scheduler (solvers, inputs and outputs)
+my_heccer.SetTimeStep(1e-06)
+
+
+#
+# Create Outputs
+#
+my_output = scheduler.CreateOutput('My output object', 'double_2_ascii')
+
+my_output.SetFilename('/tmp/purk_test_soma_aggregators')
+
+my_output.AddOutput('/purk_test_soma/segments/soma', 'Vm')
+
+scheduler.Run(steps=5000)
+
+print "Done!"
