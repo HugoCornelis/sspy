9032504282ab12ef1b1f15ff28c1d93983bae7ac
mandorodriguez+MacPro <mandorodriguez+MacPro@gmail.com>
2011-08-09 06:15:28 +0000

    1. Added a self modifier to the python PackageInfo class.

--- genesis3/sspy/__cbi__.py
+++ genesis3/sspy/__cbi__.py
@@ -22,19 +22,19 @@ __description__ = ("The simple scheduler in python is a software package that "
 __url__ = "http://genesis-sim.org"
 __download_url__ = "http://repo-genesis3.cbi.utsa.edu"
 
-def GetRevisionInfo():
+def GetRevisionInfo(self):
 # $Format: "    return \"${monotone_id}\""$
     return "b676f85605976d49c51e5db6e82c20ebc0b2a8c7"
 
-def GetPackageName():
+def GetPackageName(self):
 # $Format: "    return \"${package}\""$
     return "sspy"
 
-def GetVersion():
+def GetVersion(self):
 # $Format: "    return \"${major}.${minor}.${micro}-${label}\""$
     return "0.0.0-alpha"
 
-def GetDependencies():
+def GetDependencies(self):
     """!
     @brief Provides a list of other CBI dependencies needed.
     """
