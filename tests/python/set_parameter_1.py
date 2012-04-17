95346ab0eeed40de52a8a60886808da1e01f2490
mandorodriguez <mandorodriguez@gmail.com>
2012-04-17 20:53:21 +0000

    1. Added a test script for testing the top level SetParameter method.

---
+++ tests/python/set_parameter_1.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+"""
+
+"""
+import pdb
+import os
+import sys
+
+os.environ['NEUROSPACES_NMC_MODELS']= os.path.join('/', 'usr', 'local', 'neurospaces', 'models', 'library')
+
+from test_library import add_sspy_path
+
+add_sspy_path()
+
+from sspy import SSPy 
+
+
+scheduler = SSPy(verbose=True)
+
+my_model_container = None
+#
+# Create a model container service and load an ndf file
+#
+    
+my_model_container = scheduler.CreateService(name="My Model Container",
+                                             type="model_container",
+                                             verbose=True)
+
+my_model_container.Load('tests/cells/purk_test.ndf')
+
+my_model_container.SetParameter('/purk_test/segments/soma',
+                                'INJECT',
+                                2e-09)
+
+#
+# Must create solver.
+#
+my_heccer = scheduler.CreateSolver('My solver', 'heccer', verbose=True)
+
+# Sets the segment of the model to run from
+my_heccer.SetModelName('/purk_test')
+
+# set the timestep for the entire scheduler (solvers, inputs and outputs)
+my_heccer.SetTimeStep(2e-05)
+
+scheduler.RunPrepare()
+
+
+core_heccer = my_heccer.GetCore()
+
+print "INJECT value is %.10f" % core_heccer.GetParameter('/purk_test/segments/soma', 'INJECT')
+
+print "Setting '/purk_test/segments/soma'->'INJECT' to 3e-09"
+
+scheduler.SetParameter('/purk_test/segments/soma', 'INJECT', 3e-09)
+
+print "INJECT value is %.10f" % core_heccer.GetParameter('/purk_test/segments/soma', 'INJECT')
+
+
+print "Done!"
--- tests/specifications/heccer_addressing.t
+++ tests/specifications/heccer_addressing.t
@@ -56,6 +56,52 @@ Done!
 
 
 
+			       {
+				arguments => [
+					     ],
+				command => 'tests/python/set_parameter_1.py',
+				command_tests => [
+						  {
+						   description => "Can we get and set parameters in heccer?",
+
+						   read => "Model Container: setting parameter /purk_test/segments/soma INJECT 2e-09
+
+
+No model runtime parameters defined
+
+
+Connecting 1 solvers to 1 services
+	Connecting solver 'My solver' to service 'My Model Container'
+
+
+Compiling all solvers
+	Compiling Solver: My solver
+
+
+
+
+
+
+Scheduling all simulation objects
+	Scheduling solvers:
+		Scheduling solver 'My solver'
+
+
+Initializing all schedulees
+	Initializing Schedulee: My solver
+
+
+INJECT value is 0.0000000020
+Setting '/purk_test/segments/soma'->'INJECT' to 3e-09
+INJECT value is 0.0000000030
+Done!
+",
+
+						  },
+						 ],
+				description => "Test to see if we can change parameters from the top level of the scheduler API.",
+			       },
+
 			      ],
        description => "Checks the heccer addressing capabilities",
        name => 'heccer_addressing.t',
